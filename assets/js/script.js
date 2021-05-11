5e70fdd9981550a854fc5836a88d3caafddce112
Sylvain <sylvain.vascaut@gmail.com>
2021-05-11 22:26:51 +0200

    11052235

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,24 @@
 $(function(){
+
 /* Partie Momo */
 
 /* Partie SylV */
-var game = new Audio();
-game.src = "assets/sound/game.mp3";
+$('#cache').click(function(){
+    console.log('click)');
+    $('#regles').removeClass('d-none');
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$('#start').click(function(){
+var sound= new Audio();
+sound.src="assets/sound/game.mp3";
+sound.loop=true;
+var name=$('#player');
+console.log(name);
+    sound.play();     
+    $('h1, .fight').addClass('orangered');
+    $('#joueur').html('<h2>'+name+'</h2>');
+    $('.punk, .daft').removeClass('d-none');
+})
 
 
 
