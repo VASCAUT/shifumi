9872fc5a2956163941a9ff1ed87757ec4cf75b38
Sylvain <sylvain.vascaut@gmail.com>
2021-05-12 11:38:54 +0200

    12051138

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,36 @@
 $(function(){
+    var player;
 
-/* Partie Momo */
+    $( function() {
+    $(".draggable").draggable();
+    $(".droppable").droppable({
+        drop: function(event,ui){
+        var playerChoice = $(ui.draggable).attr('id');
+        console.log(playerChoice);}
+        })
+    });
 
-/* Partie SylV */
-
-
-$('#cache').click(function(){
+    $('#cache').click(function(){
     var steeve = new Audio();
     steeve.src = "assets/sound/steeveaustin.mp3";
     steeve.play();
     console.log('click)');
     $('#regles').removeClass('d-none');
-})
+    })
 
-$('#start').click(function(){
-var sound= new Audio();
-sound.src="assets/sound/game.mp3";
-sound.loop=true;
-var name=$('#player');
-console.log(name);
+    $('#start').click(function(){
+    var sound= new Audio();
+    sound.src="assets/sound/game.mp3";
+    sound.loop=true;
+    var name=$('#player').val();
+    player = name;
+    console.log(name);
+    console.log(player);
     sound.play();     
     $('h1, .fight').addClass('orangered');
-    $('#joueur').html('<h2>'+name+'</h2>');
+    $('#joueur').html('<h2>'+player+'</h2>');
     $('.punk, .daft').removeClass('d-none');
-})
+    })
 
 
 
